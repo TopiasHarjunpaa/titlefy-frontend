8aec870ce969684e4b11bd7fe117c3986f6f7566
Topias Harjunpää <topias.harjunpaa@gmail.com>
2023-10-29 23:01:12 +0200

    update generator path

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -24,7 +24,7 @@ const Generator = () => {
 
     const generate = async () => {
         const previous = getLastTwoWords();
-        const response = await fetch(`http://127.0.0.1:5000/generate`, {
+        const response = await fetch(`https://titlefy.onrender.com/generate`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
